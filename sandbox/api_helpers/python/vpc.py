4d165072f280ea5e3e5a358630ef0943c88757ec
kainosmaxmuyunda <max.muyunda@kainos.com>
2023-06-22 16:34:29 +0100

    Account Customization

--- sandbox/api_helpers/python/vpc.py
+++ sandbox/api_helpers/python/vpc.py
@@ -1,28 +1,35 @@
-import logging
 import boto3
-from botocorgit e.exceptions import ClientError
-import json
-AWS_REGION = 'eu-west-2'
-# logger config
-logger = logging.getLogger()
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s: %(levelname)s: %(message)s')
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
 
-def create_default_vpc():
-    """
-    Creates a default VPC in the configured availability zone.
-    """
-    try:
-        response = vpc_client.create_default_vpc()
-    except ClientError:
-        logger.exception('Could not create default vpc.')
-        raise
-    else:
-        return response
+def create_vpc():
+    ec2_client = boto3.client('ec2', region_name='eu-west-2')
+
+    # Create VPC
+    response = ec2_client.create_vpc(
+        CidrBlock='10.0.0.0/16',
+        AmazonProvidedIpv6CidrBlock=False
+    )
+    
+    vpc_id = response['Vpc']['VpcId']
+    print(f"VPC created with ID: {vpc_id}")
+    
+    # Add a name tag to the VPC
+    ec2_client.create_tags(
+        Resources=[vpc_id],
+        Tags=[
+            {
+                'Key': 'Name',
+                'Value': 'APP VPC'
+            }
+        ]
+    )
+    
+    # Enable DNS hostnames in the VPC
+    ec2_client.modify_vpc_attribute(
+        VpcId=vpc_id,
+        EnableDnsHostnames={'Value': True}
+    )
+    
+    print("VPC creation completed successfully!")
 
 if __name__ == '__main__':
-    logger.info(f'Creating default VPC...')
-    default_vpc = create_default_vpc()
-    default_vpc_id = default_vpc['Vpc']['VpcId']
-    logger.info(f'Default VPC is created with VPC ID: {default_vpc_id}')
+    create_vpc()
