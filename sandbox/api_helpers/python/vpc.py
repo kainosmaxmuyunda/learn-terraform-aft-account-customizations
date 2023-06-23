94726bb4035188c6a2996fbddeb2e63724afe767
kainosmaxmuyunda <max.muyunda@kainos.com>
2023-06-23 10:38:59 +0100

    Account Customization

--- sandbox/api_helpers/python/vpc.py
+++ sandbox/api_helpers/python/vpc.py
@@ -1,35 +1,16 @@
 import boto3
 
-def create_vpc():
-    ec2_client = boto3.client('ec2', region_name='eu-west-2')
+# create VPC
 
-    # Create VPC
-    response = ec2_client.create_vpc(
-        CidrBlock='10.0.0.0/16',
-        AmazonProvidedIpv6CidrBlock=False
-    )
-    
-    vpc_id = response['Vpc']['VpcId']
-    print(f"VPC created with ID: {vpc_id}")
-    
-    # Add a name tag to the VPC
-    ec2_client.create_tags(
-        Resources=[vpc_id],
-        Tags=[
-            {
-                'Key': 'Name',
-                'Value': 'APP VPC'
-            }
-        ]
-    )
-    
-    # Enable DNS hostnames in the VPC
-    ec2_client.modify_vpc_attribute(
-        VpcId=vpc_id,
-        EnableDnsHostnames={'Value': True}
-    )
-    
-    print("VPC creation completed successfully!")
+def client_init():
+    client = boto3.client('ec2', region_name='eu-west-2')
+    return client
 
-if __name__ == '__main__':
-    create_vpc()
+def vpc_creation(client):
+    client.create_vpc(CidrBlock='172.16.0.0/24')
+
+if __name__ == "__main__":
+    client = client_init()
+    vpc_creation(client)
+
+
