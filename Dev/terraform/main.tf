70f7c4947a622360e5a6fdca9d8d1de5b693d36b
Omeiza Ibrahim <omeiza.ibrahim@kainos.com>
2023-07-05 09:34:36 +0100

    adding vpc

--- Dev/terraform/main.tf
+++ Dev/terraform/main.tf
@@ -44,13 +44,13 @@ resource "aws_route_table" "private" {
   )
 }
 
-resource "aws_route" "private" {
-  count = length(var.private_subnet_cidr_blocks)
+# resource "aws_route" "private" {
+#   count = length(var.private_subnet_cidr_blocks)
 
-  route_table_id         = aws_route_table.private[count.index].id
-  destination_cidr_block = "0.0.0.0/0"
-  # nat_gateway_id         = aws_nat_gateway.default[count.index].id
-}
+#   route_table_id         = aws_route_table.private[count.index].id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.default[count.index].id
+# }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.default.id
